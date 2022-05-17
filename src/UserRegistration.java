200799740bb72926bfeb9c8bedae7bd8c350e287
Rohit Singh Mehra <rohit264cool@gmail.com>
2022-05-18 03:36:29 +0530

    Validate the last name

--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -9,13 +9,29 @@ public class UserRegistration {
 
     private void addUser() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter First Name");
-        String firstName = scanner.next();
-        boolean isFirstName = Pattern.matches("^[A-Z][a-z]{2}$", firstName);
-        if (isFirstName) {
-            System.out.println("First name is valid");
-        } else {
-            System.out.println("First Name is invalid");
+        System.out.println("1. First Name\n2. Last Name\n0. Exit");
+        System.out.println("Enter choice");
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case 1:
+                System.out.println("Enter First Name");
+                String firstName = scanner.next();
+                boolean isFirstName = Pattern.matches("^[A-Z][a-z]{2}$", firstName);
+                if (isFirstName)
+                    System.out.println("First name is valid");
+                else
+                    System.out.println("First Name is Invalid");
+                break;
+            case 2:
+                System.out.println("Enter Last Name");
+                String lastName = scanner.next();
+                boolean isLastName = Pattern.matches("^[A-Z][a-z]{2}$", lastName);
+                if (isLastName)
+                    System.out.println("Last name is valid");
+                else
+                    System.out.println("Last Name is Invalid");
+                break;
         }
+        while (choice != 0) ;
     }
 }
